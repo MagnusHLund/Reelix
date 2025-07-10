a6439d0a0b24620bda6c2003c6b3e36b710a298f
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-07-10 21:46:40 +0200

    refactor: :construction: Rewrote button component and almost finished it

--- reelix.frontend/reelix.frontend.website/src/components/input/Button.tsx
+++ reelix.frontend/reelix.frontend.website/src/components/input/Button.tsx
@@ -1,64 +1,24 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
-import './Button.scss'
+import './button.scss'
 import cn from 'classnames'
-import {
-  calculateElementBoxModelExcludingContentBox,
-  calculateElementTotalHeight,
-  calculateElementTotalWidth,
-} from '../../utils/htmlElementUtils'
 
-interface ButtonProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLInputElement> {
   children: React.ReactNode
   type?: 'button' | 'submit' | 'reset'
-  onClick?: () => void
   className?: string
   transparent?: boolean
 }
 
-// TODO: Rewrite to use <input> tag instead. Use div, with button component and content besides it.
 const Button: React.FC<ButtonProps> = ({
-  onClick,
   children,
   type = 'button',
   className = '',
   transparent = false,
   ...props
 }) => {
-  const containerRef = useRef<HTMLDivElement>(null)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
-  const [minSize, setMinSize] = useState({ width: 0, height: 0 })
-
-  useLayoutEffect(() => {
-    if (contentRef.current) {
-      const rect = contentRef.current.getBoundingClientRect()
-      setMinSize({ width: rect.width, height: rect.height })
-    }
-  }, [children])
-
   return (
-    <div
-      className={`button__container ${className}`}
-      ref={containerRef}
-      style={{ minWidth: minSize.width, minHeight: minSize.height }}
-    >
-      <input
-        type={type}
-        className={cn('button__input', { transparent: transparent })}
-        onClick={onClick}
-        ref={inputRef}
-        style={{
-          width:
-            minSize.width + calculateElementBoxModelExcludingContentBox(inputRef.current).width,
-          height:
-            minSize.height + calculateElementBoxModelExcludingContentBox(inputRef.current).height,
-        }}
-        {...props}
-        value='Submit'
-      />
-      <div className='button__content' ref={contentRef}>
-        {children}
-      </div>
+    <div className={cn('button__container', className, { transparent: transparent })}>
+      <input type={type} className='button__input' {...props} />
+      <div className='button__content'>{children}</div>
     </div>
   )
 }
